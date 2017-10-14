6870a50b3ce0a5b0a390728423c1cc088e5e3b4f
ismalus <ismael.cama.moumen@gmail.com>
2017-10-14 15:39:56 +0200

    diferenciacion conectados

--- lobby/counter.sh
+++ lobby/counter.sh
@@ -1,8 +1,11 @@
 function adding {
-	mac=`tail -n 1 changing_file.txt | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'`
-	if ! grep -q -i "$mac" listado_jugadores.txt; then
-		  echo jugador afegit
-		  echo "$mac" `cat nplayers` >> listado_jugadores.txt
+	phrase=`tail -n 1 changing_file.txt`
+	if grep -q -i "AP-STA-CONNECTED" "$phrase" then 
+		mac=`tail -n 1 changing_file.txt | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'`
+		if ! grep -q -i "$mac" listado_jugadores.txt; then
+			echo jugador afegit
+		  	echo "$mac" `cat nplayers` >> listado_jugadores.txt
+		fi
 	fi
 }
 
