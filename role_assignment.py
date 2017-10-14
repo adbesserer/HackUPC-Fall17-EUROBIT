22e841ad4244a74f6385095b2f910807eacf285a
Claudia M. A <claumartal@gmail.com>
2017-10-14 09:53:14 +0200

    Add files via upload

---
+++ role_assignment.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import random
+
+print('Welcome to the lobby')
+print('Introduce el numero de elementos a ser desplegado')
+n = input()
+liste = {}
+n_lobo = 20
+n_enfermera = n_policia = 10
+n_habitante = 60
+
+def assign_role(role):
+    r = random.randint(0,100)
+    if (r <= n_enfermera):
+        r = random.randint(0,100)
+        if (r <= 50):
+            role = "Enfermera"
+        else:
+            role = "Policia"
+    elif (r <= n_lobo):
+        role = "Lobo"
+    elif (r <= n_habitante):
+        role = "Habitante"
+    return role
+
+
+for x in range (0, n):
+    role = "Habitante"
+    role = assign_role(role)
+    liste[x] = role
+
+
+print "Numero de jugador y rol:"
+
+for key, value in liste.items():
+    print (key, value)
